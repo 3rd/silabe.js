b1223b44ff0f01908f6611dc0d1399eda24e23d5
Andrei Neculaesei <neculaesei@users.noreply.github.com>
2018-02-27 17:54:11 +0200

    add tests

---
+++ test/index.spec.js
@@ -0,0 +1,19 @@
+/* global describe, it, before */
+
+import chai from 'chai';
+import Silabe from '../lib/silabe.js';
+
+chai.expect();
+
+const expect = chai.expect;
+const words = require('./words')
+
+describe('Word tests', () => {
+  words.forEach(word => {
+    describe(`when input is: ${word.input}`, () => {
+      it (`should return ${word.expect}`, () => {
+        expect(Silabe(word.input)).to.deep.equal(word.expect)
+      })
+    })
+  })
+});
